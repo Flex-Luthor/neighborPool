958f1eda4ca44010707e222016c7daef27b99c16
Garland Riley Jr <garlandrileyjr@gmail.com>
2016-08-14 13:49:48 -0700

    ionic geolocation working

--- test/www/js/controllers.js
+++ test/www/js/controllers.js
@@ -5,10 +5,54 @@ angular.module('appCtrl', [])
     $location.path('/home')
   }
 })
-.controller('homeCtrl', function($scope, $window, $http, $location, $cordovaGeolocation, $ionicLoading){
-        //Setting some coordinates to get the map to render but they will change in findLocation function.
-        $scope.map = { center: { latitude: 38, longitude: -122 }, zoom: 15 };
+// .controller('homeCtrl', function($scope, $window, $http, $location, $cordovaGeolocation, $ionicLoading){
+//         //Setting some coordinates to get the map to render but they will change in findLocation function.
         
+//   
+  
+// });
+
+
+// Ionic Starter App
+ 
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+
+.controller('homeCtrl', function($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform) {
+    $scope.map = { center: { latitude: 38, longitude: -122 }, zoom: 15 };
+    $ionicPlatform.ready(function() {    
+ 
+        $ionicLoading.show({
+            template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+        });
+         
+        var posOptions = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 0
+        };
+ 
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
+             
+            var myLatlng = new google.maps.LatLng(lat, long);
+             
+            var mapOptions = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };          
+             
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
+             
+            $scope.map = map;   
+            $ionicLoading.hide();           
+             
+        }, function(err) {
+            $ionicLoading.hide();
+            console.log(err);
+        });
+    })               
 });
-
-
