8f960d32eff2044b6642f0743a67d7cd5f39574c
Garland Riley Jr <garlandrileyjr@gmail.com>
2016-08-14 18:42:26 -0700

    login to signup route is working

--- test/www/js/controllers.js
+++ test/www/js/controllers.js
@@ -4,20 +4,11 @@ angular.module('appCtrl', [])
   $scope.sendHome = function(){
     $location.path('/home')
   }
+  $scope.sendSignup = function(){
+    $location.path('/signup')
+  }
 })
-// .controller('homeCtrl', function($scope, $window, $http, $location, $cordovaGeolocation, $ionicLoading){
-//         //Setting some coordinates to get the map to render but they will change in findLocation function.
-        
-//   
-  
-// });
 
-
-// Ionic Starter App
- 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 
 .controller('homeCtrl', function($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform) {
     $scope.map = { center: { latitude: 38, longitude: -122 }, zoom: 15 };
@@ -46,7 +37,7 @@ angular.module('appCtrl', [])
             };          
              
             var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
-             
+            
             $scope.map = map;   
             $ionicLoading.hide();           
              
